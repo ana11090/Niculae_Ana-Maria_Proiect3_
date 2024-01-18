8e73df27221f49ecaacbe9a5eb530969fa930754
ANA-MARIA NICULAE <ana.niculae@ubbcluj.ro>
2024-01-18 11:53:39 +0200

    Salvare urgenta

---
+++ Niculae_Ana-Maria_Proiect3/Controllers/ManagersController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Niculae_Ana_Maria_Proiect3.Data;
+using Niculae_Ana_Maria_Proiect3.Models;
+
+namespace Niculae_Ana_Maria_Proiect3.Controllers
+{
+    public class ManagersController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public ManagersController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Managers
+        public async Task<IActionResult> Index()
+        {
+              return _context.Manageri != null ? 
+                          View(await _context.Manageri.ToListAsync()) :
+                          Problem("Entity set 'LibraryContext.Manageri'  is null.");
+        }
+
+        // GET: Managers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Manageri == null)
+            {
+                return NotFound();
+            }
+
+            var manager = await _context.Manageri
+                .FirstOrDefaultAsync(m => m.ManagerId == id);
+            if (manager == null)
+            {
+                return NotFound();
+            }
+
+            return View(manager);
+        }
+
+        // GET: Managers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Managers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ManagerId,Nume")] Manager manager)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(manager);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(manager);
+        }
+
+        // GET: Managers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Manageri == null)
+            {
+                return NotFound();
+            }
+
+            var manager = await _context.Manageri.FindAsync(id);
+            if (manager == null)
+            {
+                return NotFound();
+            }
+            return View(manager);
+        }
+
+        // POST: Managers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ManagerId,Nume")] Manager manager)
+        {
+            if (id != manager.ManagerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(manager);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ManagerExists(manager.ManagerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(manager);
+        }
+
+        // GET: Managers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Manageri == null)
+            {
+                return NotFound();
+            }
+
+            var manager = await _context.Manageri
+                .FirstOrDefaultAsync(m => m.ManagerId == id);
+            if (manager == null)
+            {
+                return NotFound();
+            }
+
+            return View(manager);
+        }
+
+        // POST: Managers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Manageri == null)
+            {
+                return Problem("Entity set 'LibraryContext.Manageri'  is null.");
+            }
+            var manager = await _context.Manageri.FindAsync(id);
+            if (manager != null)
+            {
+                _context.Manageri.Remove(manager);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ManagerExists(int id)
+        {
+          return (_context.Manageri?.Any(e => e.ManagerId == id)).GetValueOrDefault();
+        }
+    }
+}
