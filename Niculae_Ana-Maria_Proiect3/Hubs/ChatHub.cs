1bead5b2b786ce8bd2eecab38ab79696e06c82a7
ana11090 <72621705+ana11090@users.noreply.github.com>
2024-01-19 03:28:07 +0200

    Salvare 3

--- Niculae_Ana-Maria_Proiect3/Hubs/ChatHub.cs
+++ Niculae_Ana-Maria_Proiect3/Hubs/ChatHub.cs
@@ -12,21 +12,55 @@ namespace Niculae_Ana_Maria_Proiect3.Hubs
         {
             _context = context;
         }
-        
+
+        //public async Task SendMessage(string user, string message, string sarcinaId)
+        //{
+        //    var comentariu = new Comentariu
+        //    {
+        //        Text = message,
+        //        DataOra = DateTime.Now,
+        //        AutorId = 1, // Placeholder for actual AutorId
+        //        SarcinaId = 1
+        //    };
+
+        //    _context.Comentarii.Add(comentariu);
+        //    await _context.SaveChangesAsync();
+
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
         public async Task SendMessage(string user, string message, string sarcinaId)
         {
+            await BroadcastMessage(user, message);
+            await SaveComment(user, message, sarcinaId);
+        }
+
+        private async Task BroadcastMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        private async Task SaveComment(string user, string message, string sarcinaId)
+        {
+            int sarcina = Int32.Parse(sarcinaId);
             var comentariu = new Comentariu
             {
                 Text = message,
                 DataOra = DateTime.Now,
-                AutorId = 1, // Placeholder for actual AutorId
-                SarcinaId = 1
+                AutorId = sarcina, // Utilizează direct variabila int
+                SarcinaId = sarcina // Utilizează direct variabila int
             };
 
             _context.Comentarii.Add(comentariu);
-            await _context.SaveChangesAsync();
-
-            await Clients.All.SendAsync("ReceiveComment", user, message);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Console.WriteLine($"An error occurred while saving the comment: {ex.Message}");
+                // Consider how you want to handle the exception. Re-throwing or logging.
+            }
         }
 
     }
